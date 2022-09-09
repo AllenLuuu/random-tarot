92cbf6152bc84aa054e91cec21667a54acfb4790
AllenLu <allenluuu@foxmail.com>
2022-09-09 20:49:35 +0800

    feat: add spread info dialog

--- pages/select.tsx
+++ pages/select.tsx
@@ -7,20 +7,31 @@ import {
   AccordionPanel,
   AccordionIcon,
   Box,
+  Button,
   Flex,
   Input,
   IconButton,
   HStack,
   Link,
-  useDimensions,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Img,
+  Text,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
 import NextImage from "next/image";
 import flipRandom from "../public/flip-random.svg";
 import flipTarot from "../public/flip-tarot.svg";
-import { SearchIcon } from "@chakra-ui/icons";
+import { QuestionOutlineIcon, SearchIcon } from "@chakra-ui/icons";
 import spreads from "../data/spreads.json";
 import useWindowWidth from "../hooks/useWindowWidth";
+import { useState } from "react";
 
 const Select: NextPage = () => {
   const textWidth = useWindowWidth() * 0.4;
@@ -28,16 +39,41 @@ const Select: NextPage = () => {
   const tarotWidth = 0.429 * textWidth;
   const textHeight = 0.786 * tarotWidth;
 
-  // const scrollHeight;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [modalTitle, setTitle] = useState("");
+  const [modalPicture, setPicture] = useState("");
+  const [modalInfo, setInfo] = useState("");
+  const [modalPath, setPath] = useState("");
+
+  function openInfo(title: string, picture: string, info: string, path: string): void {
+    setTitle(title);
+    setPicture(picture);
+    setInfo(info);
+    setPath(path);
+    onOpen();
+  }
 
   const list = spreads.map((item) => {
     const spreads = item.spreads.map((spread) => {
       return (
-        <NextLink href={spread.link} key={spread.name}>
-          <Link>
-            <AccordionPanel pb={4}>{spread.name}</AccordionPanel>
-          </Link>
-        </NextLink>
+        <AccordionPanel pb={4} key={spread.name}>
+          <Flex justify="space-between">
+            <Box flex={1}>
+              <NextLink href={spread.link} key={spread.name}>
+                <Link>
+                  <Box w="100%">{spread.name}</Box>
+                </Link>
+              </NextLink>
+            </Box>
+            <QuestionOutlineIcon
+              onClick={() =>
+                openInfo(spread.name, spread.guide, spread.description, spread.link)
+              }
+              _hover={{ cursor: "pointer" }}
+            />
+          </Flex>
+        </AccordionPanel>
       );
     });
 
@@ -55,6 +91,7 @@ const Select: NextPage = () => {
       </AccordionItem>
     );
   });
+
   return (
     <>
       <Head>
@@ -71,15 +108,51 @@ const Select: NextPage = () => {
             />
             <NextImage src={flipTarot} width={tarotWidth} height={textHeight} />
           </Box>
-          <HStack w="100%" spacing="10px" mb={10} pos="sticky" top={0} py="10px" bgColor="white" zIndex={1}>
+          <HStack
+            w="100%"
+            spacing="10px"
+            mb={10}
+            pos="sticky"
+            top={0}
+            py="10px"
+            bgColor="white"
+            zIndex={1}
+          >
             <Input placeholder="搜索牌阵" />
-            <IconButton aria-label="Search database" icon={<SearchIcon/>} />
+            <IconButton aria-label="Search spreads" icon={<SearchIcon />} />
           </HStack>
           <Accordion defaultIndex={[0, 1, 2, 3]} allowMultiple w="100%">
             {list}
           </Accordion>
         </Flex>
       </Flex>
+
+      <Modal
+        isCentered
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{modalTitle}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Img src={modalPicture}></Img>
+            <Text>{modalInfo}</Text>
+          </ModalBody>
+          <ModalFooter>
+            <Link href={modalPath}>
+              <Button colorScheme="teal" mr={3}>
+                就它了
+              </Button>
+            </Link>
+            <Button variant="ghost" onClick={onClose}>
+              关闭
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
