c704b738d2fe0501768eb97ec84bc19567127ea3
AllenLu <allenluuu@foxmail.com>
2025-11-21 15:16:08 +0800

    chores: add local storage manager for chakra ui color mode to avoid color mode conflicts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,20 @@
+import {
+  ChakraProvider,
+  createLocalStorageManager,
+  extendTheme,
+} from "@chakra-ui/react";
+import type { StyleFunctionProps } from "@chakra-ui/styled-system";
+import { mode } from "@chakra-ui/theme-tools";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { mode } from '@chakra-ui/theme-tools'
-import type { StyleFunctionProps } from '@chakra-ui/styled-system'
-import { extendTheme } from "@chakra-ui/react";
 import "../styles/globals.css";
+
+const manager = createLocalStorageManager("random-tarot-color-mode");
 
 const theme = extendTheme({
   styles: {
     global: (props: StyleFunctionProps) => ({
       body: {
-        bg: mode('#FFFFF0', 'gray.800')(props),
+        bg: mode("#FFFFF0", "gray.800")(props),
       },
     }),
   },
@@ -17,7 +22,7 @@ const theme = extendTheme({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme} colorModeManager={manager}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
